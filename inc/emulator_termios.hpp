34e0416a027beb00a4954324f73af965c33d36db
vlah02 <m.vlahovic02@hotmail.com>
2025-09-03 21:12:31 +0200

    only 1 instance of terminal guarding, with a static atomic flag. immitating singleton behaviour without global access, only needed for TerminalDevice

--- inc/emulator_termios.hpp
+++ inc/emulator_termios.hpp
@@ -3,21 +3,32 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <cstdio>
+#include <atomic>
+#include <stdexcept>
 
 class HostTerminal {
 public:
+    HostTerminal(const HostTerminal&) = delete;
+    HostTerminal& operator=(const HostTerminal&) = delete;
+
     HostTerminal() {
-        tcgetattr(STDIN_FILENO, &orig_);
-        termios t = orig_;
-        t.c_lflag &= ~(ICANON | ECHO);
+        bool expected=false;
+        if (!active_.compare_exchange_strong(expected,true))
+            throw std::runtime_error("HostTerminal already active");
+        if (tcgetattr(STDIN_FILENO, &orig_) != 0) { active_.store(false); throw std::runtime_error("tcgetattr failed"); }
+        termios t = orig_; t.c_lflag &= ~(ICANON | ECHO);
         tcsetattr(STDIN_FILENO, TCSANOW, &t);
         int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
         fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
     }
-    ~HostTerminal() { tcsetattr(STDIN_FILENO, TCSANOW, &orig_); }
+    ~HostTerminal() { tcsetattr(STDIN_FILENO, TCSANOW, &orig_); active_.store(false); }
 
-    static int read_char_nonblock() { return ::getchar(); }
-
+    static int read_char_nonblock() {
+        unsigned char c;
+        ssize_t n = ::read(STDIN_FILENO, &c, 1);
+        return (n == 1) ? int(c) : EOF;
+    }
 private:
     termios orig_{};
+    static inline std::atomic<bool> active_{false};
 };
